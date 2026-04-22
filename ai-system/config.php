5389b771243fe596f75a69d2e6d8e879f96d0068
whatsondev <whatsonoffice@gmail.com>
2026-04-22 11:21:48 +0600

    db and necessary connection

---
+++ ai-system/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = new mysqli("localhost","root","","aiagenting");
+
+$VERIFY_TOKEN = "verify123";
+$PAGE_ACCESS_TOKEN = "YOUR_META_TOKEN";
+$OPENAI_KEY = "YOUR_OPENAI_KEY";
+$PAGE_ID = "YOUR_PAGE_ID";
+?>
